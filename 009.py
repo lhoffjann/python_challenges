5128fa0f00846f9f10bea513e74165114814411a
Lennart <hoff.lenn@gmail.com>
2022-01-22 19:30:42 +0100

    finished 009

---
+++ 009.py
@@ -0,0 +1,4 @@
+"""Question£º Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized. Suppose the following input is supplied to the program: Hello world Practice makes perfect Then, the output should be: HELLO WORLD PRACTICE MAKES PERFECT"""
+
+word = input()
+print(word.upper())
