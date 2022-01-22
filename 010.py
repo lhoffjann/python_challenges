bf1c98ec8c9c84ff7678c283e990eada5ed010d2
Lennart <hoff.lenn@gmail.com>
2022-01-22 19:51:05 +0100

    finished 010

---
+++ 010.py
@@ -0,0 +1,12 @@
+"""Question: Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all duplicate words and sorting them alphanumerically. Suppose the following input is supplied to the program: hello world and practice makes perfect and hello world again Then, the output should be: again and hello makes perfect practice world"""
+
+text = [x for x in input().split(' ')]
+
+result = []
+
+for i in text:
+    if i not in result:
+        result.append(i)
+
+result.sort()
+print(' '.join(result)) 
